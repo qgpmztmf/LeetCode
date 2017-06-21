eadc91ce859fa5c5389a922db884760db774a168
qgpmztmf <mercedes1993@163.com>
2017-06-21 21:32:52 +0800

    // 113. Path Sum II

---
+++ 113.cpp
@@ -0,0 +1,49 @@
+// 113. Path Sum II 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> ans;
+        if(!root) return ans;
+        vector<int> tmp;
+        pathSum(root, sum, ans, tmp);
+        return ans;
+    }
+    void pathSum(TreeNode* root, int sum, vector<vector<int>> &ans, vector<int> &tmp) {
+        if(!root) {
+            if(sum == 0){
+                ans.push_back(tmp);
+            }
+        }   
+         
+        else{
+            tmp.push_back(root -> val); 
+            
+            if(root -> left == NULL && root -> right == NULL){
+                pathSum(root -> left, sum - root -> val, ans, tmp);
+                tmp.pop_back();
+            }
+            else if(root -> left != NULL && root -> right == NULL){
+                pathSum(root -> left, sum - root -> val, ans, tmp);
+                tmp.pop_back();
+            }
+            else if(root -> left == NULL && root -> right != NULL){
+                pathSum(root -> right, sum - root -> val, ans, tmp);
+                tmp.pop_back();
+            }
+            else{
+                pathSum(root -> left, sum - root -> val, ans, tmp);
+                pathSum(root -> right, sum - root -> val, ans, tmp);
+                tmp.pop_back();
+            }
+        }
+    }
+};
